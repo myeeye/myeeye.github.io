e6a8675d48cc9487da8b203dd7150bb2497476b9
myeeye <1402636695@qq.com>
2023-09-11 20:11:08 +0800

    upload my blog

--- random.js
+++ random.js
@@ -4,7 +4,9 @@ pagelist = [
     '/categories/游记/','/categories/杂项/','/categories/学术/',
     '/tags/游记/','/tags/题解/','/tags/做题记录/','/tags/图片挂了/','/tags/杂项/','/tags/学术/',
     '/post/404/','/post/hello-world/',
-    '/post/HDU2022/','/post/CSP2022/','/post/WC2023/','/post/GDKOI2023/','/post/ZJOI2023/','/post/NOI2023/','/post/THUPC2023/',
+    '/post/GDKOI2021/','/post/USACO2023Jan/','/post/ABC268/','/post/USACO2023Feb/',
+    '/post/HDU2022/','/post/CSP2022/','/post/WC2023/','/post/GDKOI2023/','/post/ZJOI2023/','/post/THUPC2023/','/post/NOI2023/',
+    '/post/what-is-det/','/post/surreal-number/','/post/uoj514/','/post/loj3362/',
 ]
 
 window.location.href=pagelist[Math.floor(Math.random()*pagelist.length+1e-9)];
