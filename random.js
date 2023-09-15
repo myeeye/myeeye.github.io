1b541ead0ae08758397da662a1f9e785adff08ad
myeeye <1402636695@qq.com>
2023-09-15 21:24:56 +0800

    upload my blog

--- random.js
+++ random.js
@@ -4,7 +4,7 @@ pagelist = [
     '/categories/游记/','/categories/杂项/','/categories/学术/',
     '/tags/游记/','/tags/题解/','/tags/做题记录/','/tags/图片挂了/','/tags/杂项/','/tags/学术/',
     '/post/404/','/post/hello-world/',
-    '/post/gdkoi2021/','/post/USACO2023Jan/','/post/abc268/','/post/usaco2023feb/',
+    '/post/gdkoi2021/','/post/usaco2023jan/','/post/abc268/','/post/usaco2023feb/',
     '/post/hdu2022/','/post/csp2022/','/post/wc2023/','/post/gdkoi2023/','/post/zjoi2023/','/post/thupc2023/','/post/noi2023/',
     '/post/what-is-det/','/post/surreal-number/','/post/uoj514/','/post/loj3362/',
 ]
